02a8f9942e9ca837f1c7ae9b115e8af4606d0df5
Kaylynn <mkaylynn7@gmail.com>
2020-05-27 16:12:05 +1000

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bigbeans-kaylynn234",
-    version="1.0.0rc1",
+    version="1.0.0rc2",
     author="Kaylynn Morgan",
     author_email="mkaylynn7@gmail.com",
     description="A bad database wrapper",
